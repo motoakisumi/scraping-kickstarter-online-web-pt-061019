a2897c7b9b03142adbf46addb7eac238d4e6ba1c
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-18 07:16:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -24,8 +24,9 @@ def create_project_hash
       :location => project.css("ul.project-meta span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
-  end
   binding.pry
+  end
+
   # return the projects hash
   projects
 
